5a66c1a8f257efea384f2d77fdf42eda01285066
Alex McIvor <mcivor444@gmail.com>
2022-06-03 11:59:37 -0700

    Create funcs for round & game of 5 I might have missed a commit between creating the function that creates the round and the function that runs it's 5 times. Whoops!

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 console.log('hello world!');
 
+const shoot = 'Rock'
 
 function computerPlay () {
     let options = ["Rock", "Paper", "Scissors"];
@@ -7,4 +8,62 @@ function computerPlay () {
     return result;
 }
 
-console.log(computerPlay());
+
+function playRound (playerSelection, computerSelection) {
+    let player = playerSelection.toUpperCase();
+    let computer = computerSelection.toUpperCase();
+    if (player === "ROCK" && computer === "PAPER") {
+        return "You Lose! Paper beats Rock!";
+        }
+    else if (player === "ROCK" && computer === "ROCK") {
+        return "You Tie!";
+        }
+    else if (player === "ROCK" && computer === "SCISSORS") {
+        return "You Win! Rock beats Scissors!";
+        }
+    else if (player === "PAPER" && computer === "PAPER") {
+        return "You Tie!";
+        }
+    else if (player === "PAPER" && computer === "SCISSORS") {
+        return "You Lose! Scissors beats Paper!";
+        }
+    else if (player === "PAPER" && computer === "ROCK") {
+        return "You Win! Paper beats Rock!";
+        }
+    else if (player === "SCISSORS" && computer === "PAPER") {
+        return "You Win! Scissors beats Paper!";
+        }
+    else if (player === "SCISSORS" && computer === "ROCK") {
+        return "You Lose! Rock beats Scissors!";
+        }
+    else if (player === "SCISSORS" && computer === "SCISSORS") {
+        return "You Tie!";
+        } 
+    else {return "You didn't put in 'Rock', 'Paper', or 'Scissors', did you? You idiot!"}
+}
+
+function game () {
+ let winCount = 0;
+ let loseCount = 0;
+    for (var i = 0; i < 5; i++) {
+        let round = playRound(shoot, computerPlay());
+        console.log(round);
+        if (round.includes("Win")) {
+            winCount++;
+        } 
+        else if (round.includes("Lose")) {
+            loseCount++;
+        }
+    }
+ if (winCount > loseCount) {
+     return "You Won the game!";
+ } 
+ else if (winCount < loseCount) {
+     return "You Lost the game! Loser!";
+ } 
+ else {
+ return "You Tied!";
+ }
+}
+
+console.log(game());
